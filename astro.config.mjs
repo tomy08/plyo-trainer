a6d2aae830d237c2ef8dcaf644c5b93a1b313270
tomy08 <tomasesantacruz@gmail.com>
2025-05-24 21:01:33 -0300

    Replace node adapter import with vercel serverless adapter in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/serverless'
 export default defineConfig({
   output: 'server',
   vite: {
